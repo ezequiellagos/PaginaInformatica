01958c886793bd47b0e21cbed1699dbdd0fd7d69
Ezequiel Lagos <ezequiel.lagos@alumnos.upla.cl>
2017-07-23 20:13:27 -0400

    Sincronizacion

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,9 @@
 <div class="row">
 	<div class="col-md-12">
 		<h2>titulo</h2>
+		<p>descripcion</p>
 		<p>posted 5 horas atras</p>
 	</div>
 </div>
+<hr>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,5 @@
 */
 
 Route::get('/posts', 'PostsController@index');
+
+Route::get('/posts/{id}', 'PostsController@show');
